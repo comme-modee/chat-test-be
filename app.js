7112ec02676118c533190ae2fb25152657d5d0df
Soo Jung <comme746@naver.com>
2024-07-04 16:39:48 +0900

    add cors url

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
     cors: {
-        origin: 'http://localhost:3000'
+        origin: 'https://dev-ing-with-chat.netlify.app'
     },
 });
 
